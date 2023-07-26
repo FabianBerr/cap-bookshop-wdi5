6e0bf7e54df6f040fd3db2df1a29b5f39ab1bfb5
Fabian Berr <fabian.berr@syskron.com>
2023-07-26 11:12:58 +0200

    added test cases

--- app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/manageBooks.page.js
@@ -1,4 +1,30 @@
 module.exports = {
+
+  iClickOnCreateNewBook: async () => {
+    await browser.asControl({
+      selector: {
+        id: "books::BooksList--fe::table::Books::LineItem::StandardAction::Create",
+        interaction: {
+          idSuffix: "BDI-content"
+        }
+      }
+    }).press();
+
+  },
+
+  theBookListContains: async (sBook) => {
+    const text = await browser.asControl({
+      selector: {
+        controlType: "sap.m.Link",
+        viewName: "sap.fe.templates.ListReport.ListReport",
+        viewId: "books::BooksList",
+        properties: {
+          text: sBook
+        }
+      }
+    }).getText();
+    expect(text).toEqual(sBook);
+  }
   /**
     * define actions for the page object
   */
@@ -7,3 +33,4 @@ module.exports = {
    * define assertions for the page object
    */
 }
+
--- app/admin-books/webapp/test/wdi5/pageobjects/newBook.page.js
+++ app/admin-books/webapp/test/wdi5/pageobjects/newBook.page.js
@@ -1,4 +1,97 @@
 module.exports = {
+        iEnterTitle: async (sBook) => {
+                await browser.asControl({
+                        selector: {
+                                id: "books::BooksDetailsList--fe::EditableHeaderForm::EditableHeaderTitle::Field-edit",
+                                interaction: {
+                                        idSuffix: "inner"
+                                }
+                        }
+                }).enterText(sBook);
+        },
+
+        iSelectGenre: async () => {
+                await browser.asControl({
+                        selector: {
+                                id: "books::BooksDetailsList--fe::FormContainer::FieldGroup::General::FormElement::DataField::genre_ID::Field-edit-inner",
+                                interaction: {
+                                        idSuffix: "inner"
+                                }
+                        }
+                }).press();
+                await browser.asControl({
+                        selector: {
+                                controlType: "sap.fe.macros.controls.FieldWrapper",
+                                viewName: "sap.fe.templates.ObjectPage.ObjectPage",
+                                viewId: "books::BooksDetailsList",
+                                searchOpenDialogs: true,
+                                descendant: {
+                                        controlType: "sap.m.Text",
+                                        viewName: "sap.fe.templates.ObjectPage.ObjectPage",
+                                        viewId: "books::BooksDetailsList",
+                                        bindingPath: {
+                                                path: "/Genres(10)",
+                                                propertyPath: "name"
+                                        },
+                                        searchOpenDialogs: true
+                                }
+                        }
+                }).press();
+        },
+
+        iSelectAuthor: async () => {
+                await browser.asControl({
+                        selector: {
+                                id: "books::BooksDetailsList--fe::FormContainer::FieldGroup::General::FormElement::DataField::author_ID::Field-edit-inner-vhi"
+                        }
+                }).press();
+                await browser.asControl({
+                        selector: {
+                                controlType: "sap.fe.macros.controls.FieldWrapper",
+                                viewName: "sap.fe.templates.ObjectPage.ObjectPage",
+                                viewId: "books::BooksDetailsList",
+                                searchOpenDialogs: true,
+                                ancestor: {
+                                        id: "books::BooksDetailsList--fe::FormContainer::FieldGroup::General::FieldValueHelp::author_ID::Dialog::qualifier::::Table-innerTable-rows-row2",
+                                        searchOpenDialogs: true
+                                }
+                        }
+                }).press();
+        },
+
+        iPressCreate: async () => {
+                await browser.asControl({
+                        selector: {
+                                id: "books::BooksDetailsList--fe::FooterBar::StandardAction::Save",
+                                interaction: {
+                                        idSuffix: "BDI-content"
+                                }
+                        }
+                }).press();
+        },
+
+        iSeeEditButton: async () => {
+                const enabled = await browser.asControl({
+                        selector: {
+                                id: "books::BooksDetailsList--fe::StandardAction::Edit"
+                        }
+                }).getEnabled();
+                expect(enabled).toBeTruthy();
+        },
+
+        iNavigateBack: async () => {
+                await browser.asControl({
+                        selector: {
+                                id: "backBtn"
+                        }
+                }).press();
+        }
+
+
+
+
+
+
         /**
          * define actions for the page object
          */
